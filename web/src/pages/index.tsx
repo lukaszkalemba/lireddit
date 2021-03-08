bd9eee79a0dae11521b298494dd859192ebfc58d
Lukasz Kalemba <lukasz.kalemba@appstract.software>
2021-03-08 21:14:30 +0100

    Create posts query

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,17 @@
 import { withUrqlClient } from 'next-urql';
 import NavBar from 'components/NavBar';
 import { createUrqlClient } from 'utils/createUrqlClient';
+import { usePostsQuery } from 'generated/graphql';
 
-const Index = () => (
-  <div>
-    <NavBar />
-  </div>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+
+  return (
+    <>
+      <NavBar />
+      {data && data.posts.map(({ id, title }) => <div key={id}>{title}</div>)}
+    </>
+  );
+};
 
 export default withUrqlClient(createUrqlClient)(Index);
